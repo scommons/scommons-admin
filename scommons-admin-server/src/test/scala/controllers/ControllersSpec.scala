ad6aea9e44fb0be5b52e1ac2216705939272ce86
Viktor Podzigun <viktor.podzigun@gmail.com>
2018-06-29 10:19:08 +0200

    Fixed unit tests

--- scommons-admin-server/src/test/scala/controllers/ControllersSpec.scala
+++ scommons-admin-server/src/test/scala/controllers/ControllersSpec.scala
@@ -1,11 +1,15 @@
 package controllers
 
+import akka.actor.ActorSystem
 import akka.testkit.SocketUtil
 import com.ticketfly.play.liquibase.PlayLiquibaseModule
+import controllers.ui.CompanyController
 import modules.ApplicationModule
 import org.scalatest.{Suites, TestSuite}
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.Application
+import play.api.mvc.ControllerComponents
+import scaldi.Module
 import scaldi.play.ScaldiApplicationBuilder
 
 class ControllersSpec extends Suites(
@@ -23,6 +27,13 @@ class ControllersSpec extends Suites(
     disabled = List(
       classOf[ApplicationModule],
       classOf[PlayLiquibaseModule]
-    )
+    ),
+    modules = List(new Module {
+      private implicit lazy val ec = inject[ActorSystem].dispatcher
+      private implicit lazy val components = inject[ControllerComponents]
+      
+      //test-only
+      bind[CompanyController] to new CompanyController(null)
+    })
   ).build()
 }
