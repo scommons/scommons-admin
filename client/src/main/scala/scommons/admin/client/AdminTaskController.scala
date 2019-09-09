067dfa00a15b8c7fa3f3e966e50e9d848508a27a
Viktor Podzigun <viktor.podzigun@gmail.com>
2019-09-09 15:32:42 +0200

    Moved TaskManager component to scommons-react-redux module

--- client/src/main/scala/scommons/admin/client/AdminTaskController.scala
+++ client/src/main/scala/scommons/admin/client/AdminTaskController.scala
@@ -2,14 +2,19 @@ package scommons.admin.client
 
 import io.github.shogowada.scalajs.reactjs.React.Props
 import io.github.shogowada.scalajs.reactjs.redux.Redux.Dispatch
-import scommons.client.task.{TaskManager, TaskManagerProps}
+import scommons.client.app.AppTaskManagerUi
 import scommons.react.UiComponent
 import scommons.react.redux.BaseStateController
+import scommons.react.redux.task.{TaskManager, TaskManagerProps}
 
 object AdminTaskController
   extends BaseStateController[AdminStateDef, TaskManagerProps] {
 
-  lazy val uiComponent: UiComponent[TaskManagerProps] = TaskManager
+  lazy val uiComponent: UiComponent[TaskManagerProps] = {
+    TaskManager.uiComponent = AppTaskManagerUi
+    TaskManager.errorHandler = AppTaskManagerUi.errorHandler
+    TaskManager
+  }
 
   def mapStateToProps(dispatch: Dispatch, state: AdminStateDef, props: Props[Unit]): TaskManagerProps = {
     TaskManagerProps(state.currentTask)
--- client/src/test/scala/scommons/admin/client/AdminTaskControllerSpec.scala
+++ client/src/test/scala/scommons/admin/client/AdminTaskControllerSpec.scala
@@ -2,8 +2,7 @@ package scommons.admin.client
 
 import io.github.shogowada.scalajs.reactjs.React.Props
 import io.github.shogowada.scalajs.reactjs.redux.Redux.Dispatch
-import scommons.client.task.{TaskManager, TaskManagerProps}
-import scommons.react.redux.task.FutureTask
+import scommons.react.redux.task.{FutureTask, TaskManager, TaskManagerProps}
 import scommons.react.test.TestSpec
 
 import scala.concurrent.Future
