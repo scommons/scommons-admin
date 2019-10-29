ce92948b98112519f41712b0af8e251b331f4438
Viktor Podzigun <viktor.podzigun@gmail.com>
2019-10-29 15:45:44 +0100

    Externalized StandaloneAhcWSClient in WsApiHttpClient

--- server/src/it/scala/scommons/admin/server/AllAdminIntegrationTests.scala
+++ server/src/it/scala/scommons/admin/server/AllAdminIntegrationTests.scala
@@ -1,11 +1,13 @@
 package scommons.admin.server
 
 import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, Materializer}
 import akka.testkit.SocketUtil
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{Suites, TestSuite}
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.Application
+import play.api.libs.ws.ahc.StandaloneAhcWSClient
 import scaldi.Module
 import scaldi.play.ScaldiApplicationBuilder
 import scommons.admin.client.api.AdminUiApiClient
@@ -37,17 +39,24 @@ class AllAdminIntegrationTests extends Suites(
   private val dbAdminPass = "superadmin"
   private val dbName = "admin_db"
 
+  private var wsClient: StandaloneAhcWSClient = _
+
   implicit override lazy val app: Application = {
     val adminUiApiUrl = s"http://localhost:$port/scommons-admin/ui"
     println(s"adminUiApiUrl: $adminUiApiUrl")
 
-    val uiApiClient = new AdminUiApiClient(
-      new WsApiHttpClient(adminUiApiUrl)(ActorSystem("AdminUiApiWsClient"))
-    )
+    def uiApiClient(implicit system: ActorSystem) = {
+      implicit val materializer: Materializer = ActorMaterializer()
+
+      wsClient = StandaloneAhcWSClient()
+      new AdminUiApiClient(new WsApiHttpClient(wsClient, adminUiApiUrl)(system.dispatcher))
+    }
 
     new ScaldiApplicationBuilder(modules = List(new Module {
       //test-only
-      bind[AdminUiApiClient] to uiApiClient
+      bind[AdminUiApiClient] to uiApiClient(
+        inject[ActorSystem]
+      )
     })).configure(
       "quill.db.host" -> "localhost",
       "quill.db.port" -> postgresPort,
@@ -65,5 +74,11 @@ class AllAdminIntegrationTests extends Suites(
     initializeDb("/scommons/admin/dao/changelog/createDb.sql")
     initializeDb("/scommons/admin/dao/changelog/initialSql.sql", dbAdminUser, dbAdminPass, dbName)
     initializeDb("/test_data.sql", dbAdminUser, dbAdminPass, dbName)
+  }
+
+  override def afterAll(): Unit = {
+    wsClient.close()
+
+    super.afterAll()
   }
 }
