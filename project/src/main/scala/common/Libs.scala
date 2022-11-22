e55a710ae61831c0dec057b290f6a36ec3ac2271
Viktor Podzigun <viktor.podzigun@gmail.com>
2022-11-22 12:47:21 +0100

    Switched to release versions of dependencies

--- project/src/main/scala/common/Libs.scala
+++ project/src/main/scala/common/Libs.scala
@@ -6,11 +6,11 @@ import scommons.sbtplugin.project.CommonLibs
 
 object Libs extends CommonLibs {
 
-  val scommonsNodejsVersion = "1.0.0-SNAPSHOT"
-  val scommonsApiVersion = "1.0.0-SNAPSHOT"
-  val scommonsServiceVersion = "1.0.0-SNAPSHOT"
-  val scommonsReactVersion = "1.0.0-SNAPSHOT"
-  val scommonsClientVersion = "1.0.0-SNAPSHOT"
+  val scommonsNodejsVersion = "1.0.0"
+  val scommonsApiVersion = "1.0.0"
+  val scommonsServiceVersion = "1.0.0"
+  val scommonsReactVersion = "1.0.0"
+  val scommonsClientVersion = "1.0.0"
 
   //////////////////////////////////////////////////////////////////////////////
   // shared dependencies
